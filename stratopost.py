3fbc0d98012dc0f1cc0fa2a588a2a27cfaf5eb8a
r3v <r3v@users.noreply.github.com>
2026-09-09 12:38:08 -0700

    1.0d2 - Actually posts
    Cleaned up the variable mess.  Initial test actually works.

--- stratopost.py
+++ stratopost.py
@@ -3,7 +3,7 @@
 #
 #  DESCRIPTION: A bot for posting quotes to bluesky.
 #
-#  VERSION:     v1.0d1
+#  VERSION:     v1.0d2
 #
 #  GITHUB:      https://github.com/r3v/stratopost
 #
@@ -17,14 +17,15 @@ import os
 import json
 import random
 from atproto import Client, client_utils
+from pathlib import Path
 
 
 # Variables to edit
 # TODO: Move to yaml file
-CONFIG_DIR = "${HOME}/.config/stratopost/"  # TODO: use XDG_CONFIG_HOME if set
-QUOTE_FILE = "testQuotes.json"
+CONFIG_DIR = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config")) / "stratopost"
+QUOTE_FILE = CONFIG_DIR / "testQuotes.json"
 BSKY_ACCOUNT_ENV = "STRATOPOST_ACCOUNT"
-BSKY_APP_PASSWORD_ENV = "STRATOPOST_PASSWORD"
+BSKY_APP_PASSWORD_ENV = "STRATOPOST_APP_PASSWORD"
 
 # Retrieve bluesky account credentials from environment variables
 BSKY_ACCOUNT = os.getenv(BSKY_ACCOUNT_ENV)
